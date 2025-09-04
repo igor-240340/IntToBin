f480cc363967e84ad018ab6ee3c90a7ccd8eb2fd
igor <igor.240340@gmail.com>
2025-09-04 20:00:05 +0700

    Fix

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 
 constexpr unsigned int msb_mask = 1 << 31;
-constexpr int int_bit_count = sizeof(int) * 8;
+constexpr int int_bit_cnt = sizeof(int) * 8;
+constexpr int r_shift_amount = int_bit_cnt - 1;
 
 int main() {
 	std::string input;
@@ -15,8 +16,8 @@ int main() {
 
 		number = atoi(input.c_str());
 
-		for (int i = 0; i < int_bit_count; ++i)
-			std::cout << (static_cast<unsigned int>((number << i) & msb_mask) >> int_bit_count - 1);
+		for (int i = 0; i < int_bit_cnt; ++i)
+			std::cout << (((number << i) & msb_mask) >> r_shift_amount);
 
 		std::cout << "\n\nInput a number (or 'q' - for exit) and press Enter." << std::endl;
 	}
