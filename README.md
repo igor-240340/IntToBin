047d1297cc8181412563b30c6700f831e335e958
igor <igor.240340@gmail.com>
2025-09-04 15:27:35 +0700

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# IntToBin
+Test assignment on implementation of printing a binary representation of a signed integer.
+
+![](docs/img.png)
---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+constexpr unsigned int msb_mask = 1 << 31;
+constexpr int int_bit_count = sizeof(int) * 8;
+
+int main() {
+	std::string input;
+	int number;
+
+	std::cout << "Input a number (or 'q' - for exit) and press Enter." << std::endl;
+	while (std::cin >> input) {
+		if (input == "q" || input == "Q")
+			break;
+
+		number = atoi(input.c_str());
+
+		for (int i = 0; i < int_bit_count; ++i)
+			std::cout << (static_cast<unsigned int>((number << i) & msb_mask) >> int_bit_count - 1);
+
+		std::cout << "\n\nInput a number (or 'q' - for exit) and press Enter." << std::endl;
+	}
+
+	return 0;
+}
